ca81d67d94a3007bb8499180d78cdd391d5b86f8
hiroshige <hiroshige.negishi@arms-asia.com>
2016-08-05 00:20:29 +0700

    try failure

--- src/test/java/com/arms/SampleTest.java
+++ src/test/java/com/arms/SampleTest.java
@@ -7,6 +7,6 @@ import static org.junit.Assert.*;
 public class SampleTest {
     @Test
     public void testAdd() {
-        assertEquals(3, Sample.add(1, 2));
+        assertEquals(2, Sample.add(1, 2));
     }
 }
